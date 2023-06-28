fef41befa4ac3640781ceed0feca8982be8a402b
andersonazeved <andersonsilva14.2017@gmail.com>
2023-06-28 16:59:21 -0300

    atualização árvore binária

--- relatorio-2/fontes/1.arvore-binaria/tree.c
+++ relatorio-2/fontes/1.arvore-binaria/tree.c
@@ -101,12 +101,12 @@ int main(int argc, char **argv){
 
     /* Calculando o tempo de execução */
     /*-------------------------------------------------------------------*/
-    //clock_gettime(CLOCK_MONOTONIC, &b);
-    //search(root, rand()); // Melhor Caso
-    //search(root, n); // Pior Caso
-    //clock_gettime(CLOCK_MONOTONIC, &a);
-    //t = (a.tv_sec * 1e9 + a.tv_nsec) - (b.tv_sec * 1e9 + b.tv_nsec);
-    //printf("%u\n", t);
+    clock_gettime(CLOCK_MONOTONIC, &b);
+    search(root, rand()); // Melhor Caso
+    search(root, n); // Pior Caso
+    clock_gettime(CLOCK_MONOTONIC, &a);
+    t = (a.tv_sec * 1e9 + a.tv_nsec) - (b.tv_sec * 1e9 + b.tv_nsec);
+    printf("%u\n", t);
     /*-------------------------------------------------------------------*/
 
     /* Criando o diagrama */
